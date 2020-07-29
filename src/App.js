8b67352bc7dfb63c9c8ad43ca9833cb4ab65a142
SaurabhApraj <saurabhapraj7@gmail.com>
2020-07-29 22:57:07 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <h1>Welcome to Bootstrap</h1>
+      <button className="btn btn-success">Click</button>
+    </React.Fragment>
+  );
+};
+
+export default App;
