ee226708210075687964d9a9b1f3f89d5121f995
SaurabhApraj <saurabhapraj7@gmail.com>
2020-07-29 23:15:51 +0530

    Added cards

--- src/App.js
+++ src/App.js
@@ -4,8 +4,73 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 const App = () => {
   return (
     <React.Fragment>
-      <h1>Welcome to Bootstrap</h1>
-      <button className="btn btn-success">Click</button>
+      <h1 className="text-center my-5 text-primary text-capitalize">
+        Welcome to Bootstrap
+      </h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-sm">
+            <div class="card">
+              <img
+                src="https://picsum.photos/200/300"
+                class="card-img-top"
+                alt="..."
+                height="200px"
+              />
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="#" class="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm">
+            <div class="card">
+              <img
+                src="https://picsum.photos/200/301"
+                class="card-img-top"
+                alt="..."
+                height="200px"
+              />
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="#" class="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm">
+            <div class="card">
+              <img
+                src="https://picsum.photos/201/300"
+                class="card-img-top"
+                alt="..."
+                height="200px"
+              />
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="#" class="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </React.Fragment>
   );
 };
